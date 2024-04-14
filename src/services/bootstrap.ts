8221d167a56b978ce9536002418d16020374c348
manot40 <kevinsandiho290100@gmail.com>
2024-04-14 22:28:18 +0700

    Fix: improve bootstrap

--- src/services/bootstrap.ts
+++ src/services/bootstrap.ts
@@ -1,16 +1,15 @@
 import type { User } from 'grammy/types';
 
+import { eq } from 'drizzle-orm';
 import { user$ } from '~/db/schema';
 
 export async function registerUser(user: User) {
   const username = user.username || null;
   const fullname = [user.first_name, user.last_name].filter(Boolean).join(' ');
 
-  const [dbUser] = await db
-    .insert(user$)
-    .values({ teleId: user.id, username, fullname })
-    .onConflictDoUpdate({ set: { username, fullname }, target: user$.teleId })
-    .returning();
+  const userData = await db.query.user$.findFirst({ where: eq(user$.teleId, user.id) });
+  if (userData) return userData;
 
+  const [dbUser] = await db.insert(user$).values({ teleId: user.id, username, fullname }).returning();
   return dbUser;
 }
